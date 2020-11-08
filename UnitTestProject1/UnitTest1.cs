7c1056db7cd63a4ade745cf92a9f09b93da5f4e8
CMD <gautamkailashsingh@gmail.com>
2020-11-08 23:56:45 +0530

    Add project files.

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,28 @@
+using EmployeeManagement;
+using EmployeeManagement.Model.SalaryModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GivenSalaryDataAbleToUpdateSalaryDetails()
+        {
+            Salary salary = new Salary();
+            SalaryUpdateModel updateModel = new SalaryUpdateModel()
+            {
+                salaryId = 5,
+                Month = "Jan",
+                EmployeeSalary = 1500,
+                EmployeeId = 5
+            };
+
+            int EmpSalary = salary.UpdateEmployeeSalary(updateModel);
+
+            Assert.AreEqual(updateModel.EmployeeSalary, EmpSalary);
+        }
+    }
+
+}
